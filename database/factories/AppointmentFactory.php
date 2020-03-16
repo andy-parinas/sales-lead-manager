453fa2f5b7bc5a2e5a0fc1e4054b61735dd3106a
Andy Parinas <parinasadolfo@gmail.com>
2020-03-16 11:25:00 +1000

    Created Appointment model, migration, factory creted realtionship between Lead and Appointment, Lead and JobType created unit test

---
+++ database/factories/AppointmentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Appointment;
+use Faker\Generator as Faker;
+
+$factory->define(Appointment::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
