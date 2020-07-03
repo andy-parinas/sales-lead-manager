a779866f5a39c8a8a6b8ceeb821308588fa603ff
Andy Parinas <parinasadolfo@gmail.com>
2020-07-03 23:17:59 +1000

    Updating Contract Variations

--- app/Http/Controllers/Contract/ContractVariationController.php
+++ app/Http/Controllers/Contract/ContractVariationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Contract;
 
 use App\Contract;
+use App\ContractVariation;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -104,13 +105,66 @@ class ContractVariationController extends ApiController
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $contractId, $variationId)
     {
-        //
+        $contract = Contract::findOrFail($contractId);
+        $variation = ContractVariation::findOrFail($variationId);
+
+        $data = $this->validate($request, [
+            'variation_date' => 'sometimes|date',
+            'description' => '',
+            'amount' => 'sometimes|numeric',
+        ]);
+
+
+        DB::beginTransaction();
+
+        try {
+
+            // Adjust the Contract total before the old variation is applied
+            $total_contract = $contract->total_contract - $variation->amount;
+
+            // Apply the updated variation amount
+            $total_contract = $total_contract + $data['amount'];
+
+            //Adjust the total variation before the old variation is applied
+            $total_variation = $contract->total_variation - $variation->amount;
+
+            // Apply the updated variation amount
+            $total_variation = $total_variation + $data['amount'];
+
+
+            //Apply the updates on Contract
+            $contract->update([
+                'total_contract' => $total_contract,
+                'total_variation' => $total_variation
+            ]);
+
+
+            //Apply the updates on the Variation
+            $variation->update($data);
+
+            /**
+             * Todo Adjust the Finance HEre
+             */
+
+            DB::commit();
+
+            return $this->showOne(new ContractVariationResource($variation));
+
+        } catch (\Exception $exception){
+
+            DB::rollBack();
+
+            throw new \Exception($exception);
+
+        }
+
     }
 
     /**
