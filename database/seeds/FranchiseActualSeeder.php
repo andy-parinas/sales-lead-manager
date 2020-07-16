0b823acf8b0f7f7853c7b6ce602e203b8fad7039
Andy Parinas <parinasadolfo@gmail.com>
2020-07-17 08:37:37 +1000

    Created FranchiseActualSeeder.php and Staged refactor for LeadSeeder.php

---
+++ database/seeds/FranchiseActualSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Franchise;
+use Illuminate\Database\Seeder;
+
+class FranchiseActualSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $franchiseFile = storage_path() . '/app/database/franchise.json';
+
+        $strJsonFileContents = file_get_contents($franchiseFile);
+        $array = json_decode($strJsonFileContents, false);
+
+        $franchises = $array->franchises;
+
+
+        foreach ($franchises as $franchise){
+
+            print ("Creating Main Franchise \n");
+            $newFranchise = Franchise::create([
+                'franchise_number' => $franchise->franchise_number,
+                'name' => $franchise->name
+            ]);
+
+            foreach ($franchise->children as $child){
+                print ("Creating Sub Franchise \n");
+                Franchise::create([
+                    'franchise_number' => $child->franchise_number,
+                    'name' => $child->name,
+                    'parent_id' => $newFranchise->id
+                ]);
+            }
+        }
+
+    }
+}
--- database/seeds/LeadSeeder.php
+++ database/seeds/LeadSeeder.php
@@ -15,43 +15,47 @@ class LeadSeeder extends Seeder
      */
     public function run()
     {
-        $designAdvisors = factory(\App\DesignAssessor::class, 20)->create();
-        $leadSourceIds = LeadSource::all()->pluck('id')->toArray();
-        $productIds = \App\Product::all()->pluck('id')->toArray();
-        $designAdvisorIds = $designAdvisors->pluck('id')->toArray();
+//        $designAdvisors = factory(\App\DesignAssessor::class, 20)->create();
+//        $leadSourceIds = LeadSource::all()->pluck('id')->toArray();
+//        $productIds = \App\Product::all()->pluck('id')->toArray();
+//        $designAdvisorIds = $designAdvisors->pluck('id')->toArray();
+//
+//        //Loop Through the postcodes for the SalesContact
+//        Postcode::all()->each(function ($postcode) use ($leadSourceIds, $productIds, $designAdvisorIds) {
+//
+//            $franchises = $postcode->franchises;
+//
+//            foreach ($franchises as $franchise){
+//                if(!$franchise->isParent()){
+//                    dump('Creating Sales Contact');
+//                    factory(SalesContact::class, 60)->create(['postcode' => $postcode->pcode])
+//                        ->each(function ($contact) use ($franchise, $leadSourceIds, $productIds, $designAdvisorIds) {
+//
+//                            dump('Creating Lead');
+//                            $leadSourceKey = array_rand($leadSourceIds);
+//                            $productKey = array_rand($productIds);
+//                            $designAdvisorKey = array_rand($designAdvisorIds);
+//
+//                            $lead = factory(Lead::class)->create([
+//                                'sales_contact_id' => $contact->id,
+//                                'franchise_id' => $franchise->id,
+//                                'lead_source_id' => $leadSourceIds[$leadSourceKey]
+//                            ]);
+//                            factory(\App\Appointment::class)->create(['lead_id' => $lead->id]);
+//                            factory(\App\JobType::class)->create([
+//                                'lead_id' => $lead->id,
+//                                'product_id' => $productIds[$productKey],
+//                                'design_assessor_id' => $designAdvisorIds[$designAdvisorKey]
+//                            ]);
+//                            factory(\App\Document::class)->create(['lead_id' => $lead->id]);
+//                        });
+//                }
+//            }
+//
+//        });
 
-        //Loop Through the postcodes for the SalesContact
-        Postcode::all()->each(function ($postcode) use ($leadSourceIds, $productIds, $designAdvisorIds) {
 
-            $franchises = $postcode->franchises;
 
-            foreach ($franchises as $franchise){
-                if(!$franchise->isParent()){
-                    dump('Creating Sales Contact');
-                    factory(SalesContact::class, 60)->create(['postcode' => $postcode->pcode])
-                        ->each(function ($contact) use ($franchise, $leadSourceIds, $productIds, $designAdvisorIds) {
-
-                            dump('Creating Lead');
-                            $leadSourceKey = array_rand($leadSourceIds);
-                            $productKey = array_rand($productIds);
-                            $designAdvisorKey = array_rand($designAdvisorIds);
-
-                            $lead = factory(Lead::class)->create([
-                                'sales_contact_id' => $contact->id,
-                                'franchise_id' => $franchise->id,
-                                'lead_source_id' => $leadSourceIds[$leadSourceKey]
-                            ]);
-                            factory(\App\Appointment::class)->create(['lead_id' => $lead->id]);
-                            factory(\App\JobType::class)->create([
-                                'lead_id' => $lead->id,
-                                'product_id' => $productIds[$productKey],
-                                'design_assessor_id' => $designAdvisorIds[$designAdvisorKey]
-                            ]);
-                            factory(\App\Document::class)->create(['lead_id' => $lead->id]);
-                        });
-                }
-            }
-
-        });
     }
+
 }
