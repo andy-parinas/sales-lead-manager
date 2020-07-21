f07fcee95e278bef6cfc6e5e60b88c2e6a98de4b
Andy Parinas <parinasadolfo@gmail.com>
2020-07-21 20:52:37 +1000

    Adjusted the Seeder. Issue on the test server

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Appointment;
+use App\Franchise;
+use App\JobType;
+use App\Lead;
+use App\LeadSource;
+use App\Postcode;
+use App\Product;
+use App\SalesContact;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
