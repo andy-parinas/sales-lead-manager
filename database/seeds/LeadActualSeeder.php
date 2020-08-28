cc6c353483151c5c51006829c70ed89e00680afc
Andy Parinas <parinasadolfo@gmail.com>
2020-08-28 21:26:33 +1000

    Updated Lead Seeder

--- database/seeds/LeadActualSeeder.php
+++ database/seeds/LeadActualSeeder.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Appointment;
 use App\Franchise;
+use App\JobType;
 use App\Lead;
 use App\LeadSource;
 use App\Postcode;
+use App\Product;
 use App\SalesContact;
+use App\SalesStaff;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
 
@@ -58,7 +62,7 @@ class LeadActualSeeder extends Seeder
                 ];
                 try {
                     $salesContact = SalesContact::create($salesContactData);
-                    print "SalesContact Created";
+                    print "SalesContact Created \n";
                 }catch (Exception $exception){
                     print "Failed Creating Sales Contact";
                     Log::error("Error Creating Sales Contact {$contactFirstName} {$contactLastName}");
@@ -99,10 +103,73 @@ class LeadActualSeeder extends Seeder
 //                    Log::error("Unable to create Lead {$data[1]}");
 //                }
 
-                Lead::create($leadData);
+                $lead = Lead::create($leadData);
                 print "####### Lead Created ######## \n";
 
 
+                /**
+                 * JobType Seeder
+                 */
+
+                $nameArray = explode(" ", trim($data[19]));
+                $designAdvisorFirstName = $nameArray[0];
+                $designAdvisorLastName = count($nameArray) > 1 ? $nameArray[1] : " ";
+
+                $designAdvisor = SalesStaff::where('first_name', $designAdvisorFirstName)
+                                    ->where('last_name', $designAdvisorLastName)->first();
+
+                if($designAdvisor){
+
+                    $product = Product::where('name', 'LIKE', '%' . trim($data[20]) . '%')->first();
+
+                    if($product == null){
+                        $product = Product::create(['name' => trim($data[20])]);
+                    }
+
+                    $jobTypeData = [
+                        'taken_by' => trim($data[15]),
+                        'date_allocated' => trim($data[17]),
+                        'sales_staff_id' => $designAdvisor->id,
+                        'lead_id' => $lead->id,
+                        'description' => trim($data[21]),
+                        'product_id' => $product->id
+                    ];
+
+                    JobType::create($jobTypeData);
+                    print "Lead Job Type Create \n";
+
+                }else {
+                    Log::alert("Sales Staff Not found {$designAdvisorFirstName}");
+                    print "Sales Staff Not found\n";
+                }
+
+                /**
+                 * Appointment Seeder
+                 */
+
+                $appointmentDate = trim($data[22]);
+                $reAppointmentDate = trim($data[24]);
+
+                $actualAppointmentDate = date("Y-m-d");
+
+                if($reAppointmentDate != "") {
+                    $actualAppointmentDate = $reAppointmentDate;
+                }elseif ($appointmentDate !== ""){
+                    $actualAppointmentDate = $appointmentDate;
+                }
+
+                $outcome = trim($data[28]) != "" ? strtolower(trim($data[28])) : "pending";
+
+                $appointmentData = [
+                    'appointment_date' => $actualAppointmentDate,
+                    'lead_id' => $lead->id,
+                    'outcome' => $outcome,
+                    'comments' => trim($data[26]),
+                    'quoted_price' => floatval(trim($data[27]))
+                ];
+
+                Appointment::create($appointmentData);
+                print "Lead Appointment Created \n";
 
             }else {
 
